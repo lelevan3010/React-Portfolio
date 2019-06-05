96c94c035c7845d53a5a791d3729f08c7997b169
Van Nguyen <93vannd@gmail.com>
2019-06-05 12:15:02 +0300

    use devicon instead of fontawesome for Skills page

--- src/components/SkillIcon/index.js
+++ src/components/SkillIcon/index.js
@@ -3,12 +3,24 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 
 function SkillIcon(props) {
+
+    // achieve name dynamically from className provided by DEVICON
+    // so this function finds the exact name from className
+    // eg. className="devicon-react-original" ==> "react"
+    const findIconName = (name) => {
+        let skillName = '';
+        let slicedName = name.slice(8);
+        let found = slicedName.indexOf('-');
+        skillName = [...slicedName].slice(0,found);
+        return skillName;
+    }
+
     return( 
         <Col>
-            <div>
-                <i className={`fab fa-${props.name} fa-10x`}></i>
+            <div> 
+                <i className={`${props.name} colored`} style={{"fontSize": "10rem"}}></i>
             </div>
-            <h3>{props.name !== 'react' ? props.name : 'react/react-native/redux'}</h3>
+            <h3>{findIconName(props.name)}</h3>
         </Col>
     )
 }
--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -6,10 +6,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 const name = [
-        ['react', 'js', 'html5'], 
-        ['sass', 'bootstrap', 'node'], 
-        ['git', 'wordpress', 'html5'], 
-        ['adobe', '', '']
+        ['devicon-react-original', 'devicon-javascript-plain', 'devicon-nodejs-plain'], 
+        ['devicon-webpack-plain', 'devicon-babel-plain', 'devicon-jquery-plain'], 
+        ['devicon-git-plain', 'devicon-mongodb-plain', 'devicon-html5-plain'], 
+        ['devicon-sass-original', 'devicon-bootstrap-plain', 'devicon-wordpress-plain'],
+        ['devicon-python-plain','devicon-cplusplus-plain','devicon-csharp-plain']
 ];
 
 const showIcons = (name) => {
