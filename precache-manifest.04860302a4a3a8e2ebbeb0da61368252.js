d69f44345d51774f660c55a1c1c868f4adb97f12
Van Nguyen <93vannd@gmail.com>
2019-06-12 22:59:46 +0300

    Updates

---
+++ precache-manifest.04860302a4a3a8e2ebbeb0da61368252.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8880f9852fa9be597bfa567f7884c8b9",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "87e75a6b1151f43205ad",
+    "url": "/React-Portfolio/static/css/main.7e5cfa59.chunk.css"
+  },
+  {
+    "revision": "2f6eec896f2969f804b6",
+    "url": "/React-Portfolio/static/js/2.619eb148.chunk.js"
+  },
+  {
+    "revision": "87e75a6b1151f43205ad",
+    "url": "/React-Portfolio/static/js/main.297f52cd.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "81eda9fa6ea520060c0183668dc8c0db",
+    "url": "/React-Portfolio/static/media/Avatar.81eda9fa.png"
+  },
+  {
+    "revision": "da46015ddb287d6c5ae86b98a045470f",
+    "url": "/React-Portfolio/static/media/Ed-sheeran.da46015d.png"
+  },
+  {
+    "revision": "baccb7967f32548dce5ca4798491f1a8",
+    "url": "/React-Portfolio/static/media/GodofWar-header.baccb796.jpg"
+  },
+  {
+    "revision": "3acdce92ea394e19ca10f857a45a718d",
+    "url": "/React-Portfolio/static/media/calculator.3acdce92.png"
+  },
+  {
+    "revision": "121dbf1cfd5ac39553adc6736bada89d",
+    "url": "/React-Portfolio/static/media/contact-manager.121dbf1c.png"
+  },
+  {
+    "revision": "8681be1032adb3ad32fbaeb60df751cf",
+    "url": "/React-Portfolio/static/media/minimal-lake-sunset-img.8681be10.jpg"
+  },
+  {
+    "revision": "a7b3ca1a7eca32e3a3c89646312f5f14",
+    "url": "/React-Portfolio/static/media/products-manager.a7b3ca1a.png"
+  },
+  {
+    "revision": "8e1fdf3dea55ab44dd46bedf4f3e025f",
+    "url": "/React-Portfolio/static/media/to-do.8e1fdf3d.png"
+  }
+]);
