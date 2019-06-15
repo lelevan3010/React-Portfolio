aa48f02224f30fc1c8b8f59c48ba351032bfde90
Van Nguyen <93vannd@gmail.com>
2019-06-15 09:49:24 +0300

    Updates

---
+++ precache-manifest.7ed8bf8592bdc0556d010e05402223f6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255f327e8a5d449f663ac22577e5d1aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e86841873cfe3672bdd",
+    "url": "/static/css/main.071c2af8.chunk.css"
+  },
+  {
+    "revision": "d17f53e05e49a41e73c3",
+    "url": "/static/js/2.ca2ac9ff.chunk.js"
+  },
+  {
+    "revision": "5e86841873cfe3672bdd",
+    "url": "/static/js/main.49a5fb8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "81eda9fa6ea520060c0183668dc8c0db",
+    "url": "/static/media/Avatar.81eda9fa.png"
+  },
+  {
+    "revision": "da46015ddb287d6c5ae86b98a045470f",
+    "url": "/static/media/Ed-sheeran.da46015d.png"
+  },
+  {
+    "revision": "baccb7967f32548dce5ca4798491f1a8",
+    "url": "/static/media/GodofWar-header.baccb796.jpg"
+  },
+  {
+    "revision": "3acdce92ea394e19ca10f857a45a718d",
+    "url": "/static/media/calculator.3acdce92.png"
+  },
+  {
+    "revision": "121dbf1cfd5ac39553adc6736bada89d",
+    "url": "/static/media/contact-manager.121dbf1c.png"
+  },
+  {
+    "revision": "8681be1032adb3ad32fbaeb60df751cf",
+    "url": "/static/media/minimal-lake-sunset-img.8681be10.jpg"
+  },
+  {
+    "revision": "a7b3ca1a7eca32e3a3c89646312f5f14",
+    "url": "/static/media/products-manager.a7b3ca1a.png"
+  },
+  {
+    "revision": "8e1fdf3dea55ab44dd46bedf4f3e025f",
+    "url": "/static/media/to-do.8e1fdf3d.png"
+  },
+  {
+    "revision": "2297e6382124467d126435a417515f0d",
+    "url": "/static/media/wordpress-portfolio.2297e638.png"
+  }
+]);
